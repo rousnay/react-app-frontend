b1b8ac762f8915f4f3135cf5e2637e3a794542ca
Mozahidur Rahman <mr.rousnay@gmail.com>
2022-08-10 14:44:05 +0600

    file uploading for channel via filepond

--- src/pages/Channel/Channel.js
+++ src/pages/Channel/Channel.js
@@ -1,8 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Grid, Box, Typography } from "@mui/material";
 import LogoSquare from "../../assets/logo-square.svg";
 
+// import ReactDOM from "react-dom";
+
+// Import React FilePond
+import { FilePond, registerPlugin, File, setOptions } from "react-filepond";
+
+// Import FilePond styles
+import "filepond/dist/filepond.min.css";
+
+// Import the Image EXIF Orientation and Image Preview plugins
+// Note: These need to be installed separately
+// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+
+// Register the plugins
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+
+const userToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQxYWY0ZWJiLTk1NWEtNDY1ZS05YzJjLTFiYWFlYzdjNjkzNSIsImVtYWlsIjoibXIucm91c25heUBnbWFpbC5jb20iLCJ1c2VyVHlwZSI6InVzZXIiLCJkZXZpY2VUeXBlIjoiaW9zIiwiZGV2aWNlVG9rZW4iOiJzdHJpbmciLCJpYXQiOjE2NTkzNjY4ODYsImV4cCI6MTY1OTM2Njk0Nn0.mAJadfoBmtF_rvFf4u7D_omcAAw6gz2n9Mkp-WmCtYA";
+
+const setServer = {
+  // url: "http://13.124.197.107:3000",
+
+  process: (
+    fieldName,
+    file,
+    metadata,
+    load,
+    error,
+    progress,
+    abort,
+    transfer,
+    options
+  ) => {
+    // fieldName is the name of the input field
+    // file is the actual file object to send
+    const formData = new FormData();
+    // formData.append(fieldName, file, file.name);
+    formData.append(fieldName, file, file.name);
+
+    formData.append("id", "aaafb550-6ef9-45cf-a8a1-2cf853410577");
+    formData.append("name", "Modified Channel");
+    formData.append("description", "Modified Desc");
+
+    const request = new XMLHttpRequest();
+
+    request.open("PUT", "http://13.124.197.107:3000/channel");
+    request.setRequestHeader("Authorization", "Bearer " + userToken);
+    // Should call the progress method to update the progress to 100% before calling load
+    // Setting computable to false switches the loading indicator to infinite mode
+    request.upload.onprogress = (e) => {
+      progress(e.lengthComputable, e.loaded, e.total);
+    };
+
+    // Should call the load method when done and pass the returned server file id
+    // this server file id is then used later on when reverting or restoring a file
+    // so your server knows which file to return without exposing that info to the client
+    request.onload = function () {
+      if (request.status >= 200 && request.status < 300) {
+        // the load method accepts either a string (id) or an object
+        load(request.responseText);
+      } else {
+        // Can call the error method if something is wrong, should exit after
+        error("oh no");
+      }
+    };
+
+    request.send(formData);
+
+    console.log(formData);
+    for (const value of formData.values()) {
+      console.log(value);
+    }
+
+    // Should expose an abort method so the request can be cancelled
+    return {
+      abort: () => {
+        // This function is entered if the user has tapped the cancel button
+        request.abort();
+
+        // Let FilePond know the request has been cancelled
+        abort();
+      },
+    };
+  },
+  revert: "./revert",
+  restore: "./restore/",
+  load: "./load/",
+  fetch: "./fetch/",
+};
+
 export default function Channel() {
+  const [image, setImage] = useState([]);
+  const [bannerImage, setBannerImage] = useState([]);
+
   return (
     <>
       <Container maxWidth="xl" sx={{}}>
@@ -24,50 +119,28 @@ export default function Channel() {
             <img src={LogoSquare} alt="Logo" />
           </Grid>
 
-          {/* <div className="">
-            <form className="" noValidate onSubmit={handleSubmit}>
-              <Grid item sm={12} md={6} sx={{ padding: "30px 30px 0 0" }}>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="countryCode"
-                  name="countryCode"
-                  label="Channel Name"
-                  type="text"
-                  onChange={(e) => setCountryCode(e.target.value)}
-                />
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="phoneNumber"
-                  name="phoneNumber"
-                  label="Mobile Number"
-                  type="text"
-                  onChange={(e) => setPhoneNumber(e.target.value)}
-                />
-              </Grid>
-              <Grid item sm={12} md={6}>
-                <p style={{ fontStyle: "italic" }}>
-                  The verification code is valid upto 10 minutes. If the input
-                  time is exceeded, press resend to receive a new verification
-                  code.
-                </p>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="logoblue"
-                  className=""
-                >
-                  Next
-                </Button>
-              </Grid>
-            </form>
-          </div> */}
+          <Grid item sm={12} md={12}>
+            <div>
+              <FilePond
+                files={image}
+                onupdatefiles={setImage}
+                allowMultiple={true}
+                maxFiles={3}
+                server={setServer}
+                name="image"
+                labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+              />
+              <FilePond
+                files={bannerImage}
+                onupdatefiles={setBannerImage}
+                allowMultiple={true}
+                maxFiles={3}
+                server={setServer}
+                name="bannerImage"
+                labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+              />
+            </div>
+          </Grid>
         </Grid>
       </Container>
     </>
