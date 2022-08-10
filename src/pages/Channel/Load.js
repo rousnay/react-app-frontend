950bd3b4c07f6a1e1d966af30e4433481aed06ed
Mozahidur Rahman <mr.rousnay@gmail.com>
2022-08-11 01:35:17 +0600

    Trying best ways to implement file uploading with preview

---
+++ src/pages/Channel/Load.js
@@ -0,0 +1,26 @@
+import "react-dropzone-uploader/dist/styles.css";
+import Dropzone from "react-dropzone-uploader";
+
+export default function Load() {
+  const getUploadParams = () => {
+    return { url: "https://httpbin.org/post" };
+  };
+
+  const handleChangeStatus = ({ meta }, status) => {
+    console.log(status, meta);
+  };
+
+  const handleSubmit = (files, allFiles) => {
+    console.log(files.map((f) => f.meta));
+    allFiles.forEach((f) => f.remove());
+  };
+
+  return (
+    <Dropzone
+      getUploadParams={getUploadParams}
+      onChangeStatus={handleChangeStatus}
+      onSubmit={handleSubmit}
+      styles={{ dropzone: { minHeight: 200, maxHeight: 250 } }}
+    />
+  );
+}
---
+++ src/pages/Channel/Restore.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import {
+  Container,
+  Grid,
+  Box,
+  Typography,
+  TextField,
+  Button,
+} from "@mui/material";
+const FileInput = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+  useEffect(() => {
+    if (selectedImage) {
+      setImageUrl(URL.createObjectURL(selectedImage));
+    }
+  }, [selectedImage]);
+  return (
+    <>
+      <input
+        accept="image/*"
+        type="file"
+        id="select-image"
+        style={{
+          display: "none",
+        }}
+        onChange={(e) => setSelectedImage(e.target.files[0])}
+      />{" "}
+      <label htmlFor="select-image">
+        <Button variant="contained" color="primary" component="span">
+          Upload Image
+        </Button>
+      </label>{" "}
+      {imageUrl && selectedImage && (
+        <Box mt={2} textAlign="center">
+          <div> Image Preview: </div>
+          <img src={imageUrl} alt={selectedImage.name} height="100px" />
+        </Box>
+      )}
+    </>
+  );
+};
+export default FileInput;
