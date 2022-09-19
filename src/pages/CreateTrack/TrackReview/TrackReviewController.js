33fd2c785411eeb558172e4ae590d65d78b3c333
Mozahidur Rahman <mr.rousnay@gmail.com>
2022-09-19 11:39:29 +0600

    Unused var removes

--- src/pages/CreateTrack/TrackReview/TrackReviewController.js
+++ src/pages/CreateTrack/TrackReview/TrackReviewController.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { API_URL } from "../../../utils/CONSTANTS";
 import { useToken } from "../../../auth/userAuth";
 import swal from "sweetalert";
